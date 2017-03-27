f124ed13bbea819307b3e4bfa6e7259c3282b571
Rob Court <robbie.court@gmail.com>
2017-03-27 11:43:58 +0100

    splitting out connection script

--- initialiseKBfromLMB.sh
+++ initialiseKBfromLMB.sh
@@ -2,13 +2,15 @@
 
 #only run once!!!!
 
-python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/ontologies_2_KB.py $KBSERVER $KBuser $KBpassword $LMBpassword 
+python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/ontologies_2_KB.py $KBSERVER $KBuser $KBpassword
 
-/opt/VFB/lmb.sh /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/load_features_from_lmb.py $KBSERVER $KBuser $KBpassword $LMBpassword
+/opt/VFB/connectToLMB.sh
 
-python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/import_individuals_lmb2KB.py $KBSERVER $KBuser $KBpassword $LMBpassword
+python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/load_features_from_lmb.py $KBSERVER $KBuser $KBpassword $LMBuser
 
-python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/add_clusters.py $KBSERVER $KBuser $KBpassword $LMBpassword
+python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/import_individuals_lmb2KB.py $KBSERVER $KBuser $KBpassword $LMBuser
 
-python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/add_annotations.py $KBSERVER $KBuser $KBpassword $LMBpassword
+python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/add_clusters.py $KBSERVER $KBuser $KBpassword $LMBuser
+
+python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/add_annotations.py $KBSERVER $KBuser $KBpassword $LMBuser
 
