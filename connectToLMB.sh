2a2ab0fb3dd604ada5073414c2b879e95ac48a74
Rob Court <robbie.court@gmail.com>
2017-03-27 11:41:32 +0100

    switching to purely a connection script

--- connectToLMB.sh
+++ connectToLMB.sh
@@ -9,22 +9,3 @@ chmod 600 ~/.ssh/*
 ssh -i $HOME/.ssh/id-rsa -4 -f -N -o ExitOnForwardFailure=yes -L 3307:127.0.0.1:3306 vfb@flybrain.mrc-lmb.cam.ac.uk
 
 sleep 5s
-
-case "$#" in
-1)  python3 $1
-    ;;
-2)  python3 $1 $2
-    ;;
-3)  python3 $1 $2 $3
-    ;;
-4)  python3 $1 $2 $3 $4
-    ;;
-5)  python3 $1 $2 $3 $4 $5
-    ;;
-6)  python3 $1 $2 $3 $4 $5 $6
-    ;;
-7)  python3 $1 $2 $3 $4 $5 $6 $7
-    ;;
-*)  python3
-    ;;
-esac
