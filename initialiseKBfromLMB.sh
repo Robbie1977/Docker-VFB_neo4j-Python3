852163fb99ef7ded2a1263e32dfce0a13568538e
Rob Court <robbie.court@gmail.com>
2017-04-06 16:43:19 +0100

    running sources first to set schema contraints

--- initialiseKBfromLMB.sh
+++ initialiseKBfromLMB.sh
@@ -2,9 +2,11 @@
 
 #only run once!!!!
 
-python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/ontologies_2_KB.py $KBSERVER $KBuser $KBpassword
-
 /opt/VFB/connectToLMB.sh
+
+python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/import_data_source.py $KBSERVER $KBuser $KBpassword $LMBuser
+
+python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/ontologies_2_KB.py $KBSERVER $KBuser $KBpassword
 
 python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/load_features_from_lmb.py $KBSERVER $KBuser $KBpassword $LMBuser
 
