bc1c77683edb84ee9cbec4abe11557b276b3ed38
Rob Court <robbie.court@gmail.com>
2017-04-27 10:19:36 +0100

    removing duplicate

--- initialiseKBfromLMB.sh
+++ initialiseKBfromLMB.sh
@@ -10,8 +10,6 @@ python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/ontologies_2_KB.py $KBSERVER $KBu
 
 python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/load_features_from_lmb.py $KBSERVER $KBuser $KBpassword $LMBuser
 
-python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/import_data_source.py $KBSERVER $KBuser $KBpassword $LMBuser
-
 python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/import_individuals_lmb2KB.py $KBSERVER $KBuser $KBpassword $LMBuser
 
 python3 /opt/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/lmb2neoKB/add_clusters.py $KBSERVER $KBuser $KBpassword $LMBuser
