4045a39d7a2accf5aa2282ffb9e048cf44613a5c
Rob Court <robbie.court@gmail.com>
2017-06-15 13:41:59 +0100

    typo fix

--- connectToLMB.sh
+++ connectToLMB.sh
@@ -2,12 +2,12 @@
 
 if [ ! -e $HOME/.ssh/id_rsa ]; then
   # copy authentication (only available on VFB servers)
-  curl -o "$HOME/.ssh/id-rsa" "$FILESERVER:/lmb/.ssh/id_rsa"
-  curl -o "$HOME/.ssh/id-rsa.pub" "$FILESERVER:/lmb/.ssh/id_rsa.pub"
+  curl -o "$HOME/.ssh/id_rsa" "$FILESERVER:/lmb/.ssh/id_rsa"
+  curl -o "$HOME/.ssh/id_rsa.pub" "$FILESERVER:/lmb/.ssh/id_rsa.pub"
   curl -o "$HOME/.ssh/known_hosts" "$FILESERVER:/lmb/.ssh/known_hosts"
 fi
 chmod 600 ~/.ssh/*
 
-ssh -i $HOME/.ssh/id-rsa -4 -f -N -o ExitOnForwardFailure=yes -L 3307:127.0.0.1:3306 vfb@flybrain.mrc-lmb.cam.ac.uk
+ssh -i $HOME/.ssh/id_rsa -4 -f -N -o ExitOnForwardFailure=yes -L 3307:127.0.0.1:3306 vfb@flybrain.mrc-lmb.cam.ac.uk
 
 sleep 5s
